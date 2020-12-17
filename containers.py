d96ce26f61e23e4ad70f9ab9212c1e3b504c6565
Marin Petrunić <marin.petrunic@gmail.com>
2020-12-17 15:15:01 +0100

    add lightclient helpers

--- containers.py
+++ containers.py
@@ -1,23 +1,10 @@
 from remerkleable.core import Path
 from test_types import TestContainer
 from helpers import print_test_case
+from eth2spec.lightclient_patch.spec import BeaconState
 
 testContainerAnchor = Path(TestContainer)
 
-print_test_case(testContainerAnchor)
-print_test_case(testContainerAnchor / 'uint')
-print_test_case(testContainerAnchor / 'bool')
-print_test_case(testContainerAnchor / 'bytes32')
-print_test_case(testContainerAnchor / 'bitlist')
-print_test_case(testContainerAnchor / 'bitvector')
-print_test_case(testContainerAnchor / 'basicList')
-print_test_case(testContainerAnchor / 'basicList' / 0)
-print_test_case(testContainerAnchor / 'basicVector')
-print_test_case(testContainerAnchor / 'basicVector' / 4)
-print_test_case(testContainerAnchor / 'complexList')
-print_test_case(testContainerAnchor / 'complexList' / 4)
-print_test_case(testContainerAnchor / 'complexList' / 4 / 'field')
-print_test_case(testContainerAnchor / 'complexVector')
-print_test_case(testContainerAnchor / 'complexVector' / 4)
-print_test_case(testContainerAnchor / 'complexVector' / 4 / 'field')
+print_test_case(Path(BeaconState)/'next_sync_committee')
+print_test_case(Path(BeaconState)/'finalized_checkpoint'/'root')
 
